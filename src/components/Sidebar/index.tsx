e32998796c66e8313a98b15f6886bfa4b77c57ea
Paulo Teixeira <paulo.teixeira@fortbrasil.com.br>
2022-12-24 18:13:30 -0300

    feature: transition button

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,25 @@
+import {useState} from 'react'
+
 import {Container} from './styles'
 
 import menuImg from '../../assets/menu.svg'
 import {ReactComponent as BurgerIcon} from '../../assets/burger.svg'
+import {ReactComponent as PizzaIcon} from '../../assets/pizza.svg'
+import {ReactComponent as SodaPopIcon} from '../../assets/soda.svg'
+import {ReactComponent as IceCreamIcon} from '../../assets/ice-cream.svg'
+
 
 export function Sidebar() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
-    <Container>
-      <button type='button'>
+    <Container isMenuOpen={menuOpen}>
+      <button type='button' onClick={handleToggleMenu}>
         <img src={menuImg} alt='Abrir e fechar o menu' />
       </button>
       <nav>
@@ -15,6 +28,24 @@ export function Sidebar() {
             <a href='#' className='active'>
               <BurgerIcon />
               <span>Hambúrgueres</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <PizzaIcon />
+              <span>Pizzas</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <SodaPopIcon />
+              <span>Bebidas</span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <IceCreamIcon />
+              <span>sobremesas</span>
             </a>
           </li>
         </ul>
